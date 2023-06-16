496f05b9547e23fa44622d00c2c7dd088d157f28
Ari Karhunen <karhuari@ul005163.eu.tieto.com>
2023-06-17 00:05:44 +0300

    saml auth test api

--- src/rasenmaeher_api/settings.py
+++ src/rasenmaeher_api/settings.py
@@ -133,6 +133,13 @@ class Settings(BaseSettings):  # pylint: disable=too-few-public-methods
     with environment variables.
     """
 
+    class Config:  # pylint: disable=too-few-public-methods
+        """Configuration of settings."""
+
+        env_file = ".env"
+        env_prefix = "RM_"
+        env_file_encoding = "utf-8"
+
     host: str = "127.0.0.1"
     port: int = 8000
     # quantity of workers for uvicorn
@@ -235,12 +242,42 @@ class Settings(BaseSettings):  # pylint: disable=too-few-public-methods
                                         WHERE work_id_hash='{work_id_hash}' OR work_id='{work_id}'
                                     ;"""
 
-    class Config:  # pylint: disable=too-few-public-methods
-        """Configuration of settings."""
-
-        env_file = ".env"
-        env_prefix = "RM_"
-        env_file_encoding = "utf-8"
+    # https://github.com/SAML-Toolkits/python3-saml/tree/master
+    saml_settings = {
+        "strict": False,  # can set to True to see problems such as Time skew/drift
+        "debug": True,
+        "idp": {
+            "entityId": "test-saml-client",
+            "singleSignOnService": {
+                "url": "http://127.0.0.1:8080/auth/realms/test/protocol/saml",
+                "binding": "urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect",
+            },
+            "x509cert": "MIIClzCCAX8CBgF6A0sAhDANBgkqhkiG9w0BAQsFADAPMQ0wCwYDVQQDDAR0ZXN0MB4XDTIxMDYxMzAyNTMwNFo\
+XDTMxMDYxMzAyNTQ0NFowDzENMAsGA1UEAwwEdGVzdDCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAK97NlCcNOhtH0a0wz5bo\
+YKb7TaxogxnlyysOWUre1uI8SC6EBV3G5DHMdg4aWXwuXwy61+JJu70xNzJj155MJ+atGS7eLrxxGl0DNoLu/LU7Vhht+j09MZt5J60DnS7\
+6H3pkvzAtRfd1P/d5JEFzWYkI4drBJccYX/nrrx2KZBkXOjwjVcEhsyK5ykA0LX+M+yFDy2w8qEWhxHuSL6enzw8IZ7qdtsF8SHqw7cdCgCJU6\
++0dxaRAAqmzMkO7BDEkwCJl0M8VaOPGo/SnZIAMYHLIUg1x0h/ecST4NPdqAwgDGtWAcD+Gp7Lr7xfBbKKqnLfg2PJdjs7Z0+NFOeVTvcC\
+AwEAATANBgkqhkiG9w0BAQsFAAOCAQEAeJ2r2yoaQAo6v8MC6iAobOeJoBoezQg/OSQqeA9lygMWmGHpDIjSV7m3PCXwf5H9/NpHgBLt8y5\
+PcjEs99uPfPeUBV/qitTFMuznMyr35e60iaHSdhZVjyCmrKgnIuGa07lng2wFabtpijqzbQJ99kYsWxbBDgbdVnt3jxohG1KKaXkGMyy7suwP\
+gwrbwXfDrpyyj33NT/Dk/2W4Fjrjg8rIkuQypwB0SQLG1cZL9Z2AgW39JeVnP/sOH1gNpCCQwbpgE9hEed80jsYWlvucnFm2aHBtGV+/7/7N3q\
+    RRpIvzrNVJoznqDDWU41RxS0NphAwX2ZqprWvN+SCEEhPGGQ==",
+        },
+        "sp": {
+            "entityId": "test-saml-client",
+            "assertionConsumerService": {
+                "url": "http://127.0.0.1:3000/api/saml/callback",
+                "binding": "urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST",
+            },
+            "x509cert": "MIIClzCCAX8CBgF6A0sAhDANBgkqhkiG9w0BAQsFADAPMQ0wCwYDVQQDDAR0ZXN0MB4XDTIxMDYxMzAyNTMwNFoXDTMxM\
+DYxMzAyNTQ0NFowDzENMAsGA1UEAwwEdGVzdDCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAK97NlCcNOhtH0a0wz5boYKb7TaxogxnlyysOW\
+Ure1uI8SC6EBV3G5DHMdg4aWXwuXwy61+JJu70xNzJj155MJ+atGS7eLrxxGl0DNoLu/LU7Vhht+j09MZt5J60DnS76H3pkvzAtRfd1P/d5JEFzWYkI4d\
+rBJccYX/nrrx2KZBkXOjwjVcEhsyK5ykA0LX+M+yFDy2w8qEWhxHuSL6enzw8IZ7qdtsF8SHqw7cdCgCJU6+0dxaRAAqmzMkO7BDEkwCJl0M8VaOPGo/S\
+nZIAMYHLIUg1x0h/ecST4NPdqAwgDGtWAcD+Gp7Lr7xfBbKKqnLfg2PJdjs7Z0+NFOeVTvcCAwEAATANBgkqhkiG9w0BAQsFAAOCAQEAeJ2r2yoaQA\
+o6v8MC6iAobOeJoBoezQg/OSQqeA9lygMWmGHpDIjSV7m3PCXwf5H9/NpHgBLt8y5PcjEs99uPfPeUBV/qitTFMuznMyr35e60iaHSdhZVjyCmrK\
+gnIuGa07lng2wFabtpijqzbQJ99kYsWxbBDgbdVnt3jxohG1KKaXkGMyy7suwPgwrbwXfDrpyyj33NT/Dk/2W4Fjrjg8rIkuQypwB0SQLG1cZL\
+9Z2AgW39JeVnP/sOH1gNpCCQwbpgE9hEed80jsYWlvucnFm2aHBtGV+/7/7N3qRRpIvzrNVJoznqDDWU41RxS0NphAwX2ZqprWvN+SCEEhPGGQ==",
+        },
+    }
 
 
 settings = Settings()
--- src/rasenmaeher_api/web/api/utils/views.py
+++ src/rasenmaeher_api/web/api/utils/views.py
@@ -1,11 +1,100 @@
 """Utils API views."""
-from fastapi import APIRouter
+from typing import Any, Dict
+from fastapi import APIRouter, Request
+from fastapi.responses import RedirectResponse
+
+# https://pypi.org/project/xmlsec/
+# poetry add python3-saml
+# poetry add xmlsec
+
+from onelogin.saml2.auth import OneLogin_Saml2_Auth
+
+# from onelogin.saml2.settings import OneLogin_Saml2_Settings
+# from onelogin.saml2.utils import OneLogin_Saml2_Utils
+
 from rasenmaeher_api.web.api.utils.schema import LdapConnString, KeyCloakConnString
 
 from ....settings import settings
 
 
 router = APIRouter()
+
+
+# https://github.com/SAML-Toolkits/python3-saml/tree/master
+
+saml_settings = settings.saml_settings
+
+
+async def prepare_from_fastapi_request(request, debug: bool = False):
+    """asd testing saml stuff..."""
+
+    if debug is True:
+        print("TODO some debug stuff maybe....")
+
+    form_data = await request.form()
+    returnvalue: Dict[Any, Any] = {
+        "http_host": request.client.host,
+        "server_port": request.url.port,
+        "script_name": request.url.path,
+        "post_data": {},
+        "get_data": {}
+        # Advanced request options
+        # "https": "",
+        # "request_uri": "",
+        # "query_string": "",
+        # "validate_signature_from_qs": False,
+        # "lowercase_urlencoding": False
+    }
+    if request.query_params:
+        returnvalue["get_data"] = (request.query_params,)
+    if "SAMLResponse" in form_data:
+        saml_response = form_data["SAMLResponse"]
+        returnvalue["post_data"]["SAMLResponse"] = saml_response
+    if "RelayState" in form_data:
+        relay_state = form_data["RelayState"]
+        returnvalue["post_data"]["RelayState"] = relay_state
+    return returnvalue
+
+
+@router.post("/saml/test")
+# async def test(request: Request, p1: Optional[str] = Form(None), p2: Optional[str] = Form(None)):
+async def test(request: Request):
+    """asd testing saml test uri"""
+    req = await prepare_from_fastapi_request(request)
+    return req
+
+
+@router.get("/saml/login")
+async def saml_login(request: Request):
+    """asd testing saml login"""
+    req = await prepare_from_fastapi_request(request)
+    auth = OneLogin_Saml2_Auth(req, settings.saml_settings)
+    # saml_settings = auth.get_settings()
+    # metadata = saml_settings.get_sp_metadata()
+    # errors = saml_settings.validate_metadata(metadata)
+    # if len(errors) == 0:
+    #   print(metadata)
+    # else:
+    #   print("Error found on Metadata: %s" % (', '.join(errors)))
+    callback_url = auth.login()
+    response = RedirectResponse(url=callback_url)
+    return response
+
+
+@router.post("/saml/callback")
+async def saml_login_callback(request: Request):
+    """asd testing saml callback"""
+    req = await prepare_from_fastapi_request(request, True)
+    auth = OneLogin_Saml2_Auth(req, settings.saml_settings)
+    auth.process_response()  # Process IdP response
+    errors = auth.get_errors()  # This method receives an array with the errors
+    if len(errors) == 0:
+        # This check if the response was ok and the user data retrieved or not (user authenticated)
+        if not auth.is_authenticated():
+            return "user Not authenticated"
+        return "User authenticated"
+    print("Error when processing SAML Response: %s %s" % (", ".join(errors), auth.get_last_error_reason()))
+    return "Error in callback"
 
 
 @router.get("/ldap-conn-string")
