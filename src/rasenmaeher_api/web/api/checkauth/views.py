50171d3b882e8db0a0b3cbbc2d0d49e4792f3cb5
Eero af Heurlin <eero.afheurlin@iki.fi>
2023-07-02 00:35:42 +0300

    cast the request state property since reflecting the type does not work

--- src/rasenmaeher_api/web/api/checkauth/views.py
+++ src/rasenmaeher_api/web/api/checkauth/views.py
@@ -14,25 +14,25 @@ router = APIRouter()
 @router.get("/jwt", dependencies=[Depends(JWTBearer(auto_error=True))])
 async def return_jwt_payload(request: Request) -> JWTPayload:
     """Method for testing JWT"""
-    return request.state.jwt
+    return cast(JWTPayload, request.state.jwt)
 
 
 @router.get("/mtls", dependencies=[Depends(MTLSHeader(auto_error=True))])
 async def return_mtls_payload(request: Request) -> DNDict:
     """Method for testing mTLS"""
-    return request.state.mtlsdn
+    return cast(DNDict, request.state.mtlsdn)
 
 
 @router.get("/mtls_or_jwt", dependencies=[Depends(MTLSorJWT(auto_error=True))])
 async def return_mtlsjwt_payload(request: Request) -> MTLSorJWTPayload:
     """Method for testing mTLS and JWT auth"""
-    return request.state.mtls_or_jwt
+    return cast(MTLSorJWTPayload, request.state.mtls_or_jwt)
 
 
 @router.get("/mtls_or_jwt/permissive", dependencies=[Depends(MTLSorJWT(auto_error=True, disallow_jwt_sub=[]))])
 async def return_mtlsjwt_payload_permissive(request: Request) -> MTLSorJWTPayload:
     """Method for testing mTLS and JWT auth, do not disallow any subjects"""
-    return request.state.mtls_or_jwt
+    return cast(MTLSorJWTPayload, request.state.mtls_or_jwt)
 
 
 @router.get("/ldap/connection-string", dependencies=[Depends(JWTBearer())])
