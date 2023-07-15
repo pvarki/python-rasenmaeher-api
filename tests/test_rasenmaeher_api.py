92412c468943891d5aab1ead894cffdd20a93b28
Eero af Heurlin <eero.afheurlin@iki.fi>
2023-07-15 14:27:37 +0300

    add test for loading the openapi spec

--- tests/test_rasenmaeher_api.py
+++ tests/test_rasenmaeher_api.py
@@ -1,7 +1,23 @@
 """Package level tests"""
+from typing import Any, Dict
+
+import pytest
+from async_asgi_testclient import TestClient
+
+
 from rasenmaeher_api import __version__
 
 
 def test_version() -> None:
     """Make sure version matches expected"""
     assert __version__ == "0.3.1"
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("app_client", [{"test": "value", "xclientcert": False}], indirect=True)
+async def test_get_openapi_json(app_client: TestClient) -> None:
+    """Check that we can get the openapi spec"""
+    resp = await app_client.get("/api/openapi.json")
+    resp_dict: Dict[Any, Any] = resp.json()
+    assert resp.status_code == 200
+    assert len(resp_dict) > 0
