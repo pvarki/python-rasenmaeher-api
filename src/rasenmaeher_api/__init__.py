e9cb48b849a55cc0f0981fb556479aeadc6f86cb
Eero af Heurlin <eero.afheurlin@iki.fi>
2023-08-26 16:23:01 +0300

    bump version and add myself to authors

--- src/rasenmaeher_api/__init__.py
+++ src/rasenmaeher_api/__init__.py
@@ -1,2 +1,2 @@
 """ python-rasenmaeher-api """
-__version__ = "0.3.4"  # NOTE Use `bump2version --config-file patch` to bump versions correctly
+__version__ = "0.4.0"  # NOTE Use `bump2version --config-file patch` to bump versions correctly
