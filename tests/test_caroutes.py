c685564ba3207a76da5d4974d89ce041e418b9ab
Eero af Heurlin <eero.afheurlin@iki.fi>
2023-08-31 22:30:57 +0300

    make fixture for signed mtls cert, add test for re-using nonce (xfail)

--- tests/test_caroutes.py
+++ tests/test_caroutes.py
@@ -1,14 +1,28 @@
 """Test the CA routes"""
+from typing import Tuple, Generator
 from pathlib import Path
 import logging
+import asyncio
 
 import pytest
+import pytest_asyncio
 from OpenSSL import crypto  # FIXME: use cryptography instead of pyOpenSSL
 from async_asgi_testclient import TestClient  # pylint: disable=import-error
+
+from rasenmaeher_api.web.api.product.views import sign_csr, get_ca
 
 LOGGER = logging.getLogger(__name__)
 
 # pylint: disable=W0621
+
+
+@pytest.fixture(scope="module")
+def event_loop() -> Generator[asyncio.AbstractEventLoop, None, None]:
+    """Module scoped event loop so we don't have to regenrate keys etc for every test"""
+    policy = asyncio.get_event_loop_policy()
+    loop = policy.new_event_loop()
+    yield loop
+    loop.close()
 
 
 @pytest.fixture(scope="module")
@@ -64,11 +78,34 @@ def test_have_csrfile(csrfile: Path) -> None:
     assert csrfile.exists()
 
 
-@pytest.mark.asyncio
-async def test_sign(csrfile: Path, kraftwerk_jwt_client: TestClient) -> None:
-    """Test signing once"""
+@pytest_asyncio.fixture(scope="module")
+async def mtlsfiles(csrfile: Path) -> Tuple[Path, Path, Path]:
+    """Return cert, key and ca cert paths, this will sign the CSR again every time due to fixture scoping issues"""
+    privkeypath = csrfile.parent.parent / "private" / "mtlsclient.key"
+    assert privkeypath.exists()
     certpath = csrfile.parent / "mtlsclient.pem"
     capath = csrfile.parent / "mtlsca.pem"
+    capem = (await get_ca()).replace("\\n", "\n")
+    LOGGER.debug("capem={}".format(capem))
+    certpem = (await sign_csr(csrfile.read_text())).replace("\\n", "\n")
+    LOGGER.debug("certpem={}".format(certpem))
+    capath.write_text(capem)
+    certpath.write_text(certpem)
+    return certpath, privkeypath, capath
+
+
+@pytest.mark.asyncio
+async def test_have_mtlscert(mtlsfiles: Tuple[Path, Path, Path]) -> None:
+    """Test that we got the files"""
+    certpath, privkeypath, capath = mtlsfiles
+    assert certpath.exists()
+    assert privkeypath.exists()
+    assert capath.exists()
+
+
+@pytest.mark.asyncio
+async def test_sign(csrfile: Path, kraftwerk_jwt_client: TestClient) -> None:
+    """Test signing"""
     client = kraftwerk_jwt_client
     resp = await client.post(
         "/api/v1/product/sign_csr",
@@ -79,10 +116,33 @@ async def test_sign(csrfile: Path, kraftwerk_jwt_client: TestClient) -> None:
     LOGGER.debug("Got response {}".format(resp))
     resp.raise_for_status()
     payload = resp.json()
-    LOGGER.debug("payload={}".format(payload))
-    assert "ca" in payload
-    capem = payload["ca"].replace("\\n", "\n")
-    capath.write_text(capem)
     assert "certificate" in payload
-    certpem = payload["certificate"].replace("\\n", "\n")
-    certpath.write_text(certpem)
+    assert "ca" in payload
+
+
+@pytest.mark.xfail(reason="Nonce checking not implemented yet")
+@pytest.mark.asyncio
+async def test_sign_twice(csrfile: Path, kraftwerk_jwt_client: TestClient) -> None:
+    """Test using same nonce twice, should fail"""
+    # First signing, should work fine
+    client = kraftwerk_jwt_client
+    resp = await client.post(
+        "/api/v1/product/sign_csr",
+        json={
+            "csr": csrfile.read_text(),
+        },
+    )
+    LOGGER.debug("Got response {}".format(resp))
+    resp.raise_for_status()
+    payload = resp.json()
+    assert "certificate" in payload
+
+    # Second signing, should return 401
+    resp2 = await client.post(
+        "/api/v1/product/sign_csr",
+        json={
+            "csr": csrfile.read_text(),
+        },
+    )
+    LOGGER.debug("Got second response {}".format(resp2))
+    assert resp2.status_code == 401
