71d1c832a0b591539d100f6a2d29d47224de104f
Eero af Heurlin <eero.afheurlin@iki.fi>
2023-09-01 00:26:23 +0300

    attempt to setup pytest-httpserver for requiring mTLS, fails in weird ways

--- tests/tlstests/conftest.py
+++ tests/tlstests/conftest.py
@@ -1,12 +1,18 @@
 """mTLS fixtures"""
 from typing import Tuple, Generator
+import random
 from pathlib import Path
 import logging
 import asyncio
+import ssl
 
 import pytest
 import pytest_asyncio
+import aiohttp
 from OpenSSL import crypto  # FIXME: use cryptography instead of pyOpenSSL
+from libpvarki.mtlshelp import get_ssl_context, get_session
+from pytest_httpserver import HTTPServer
+
 
 from rasenmaeher_api.web.api.product.views import sign_csr, get_ca
 
@@ -57,7 +63,7 @@ def csrfile(datadir: Path, keypair: crypto.PKey) -> Path:
     """Generate CSR file"""
     csrpath = datadir / "public" / "mtlsclient.csr"
     req = crypto.X509Req()
-    req.get_subject().CN = "product.localmaeher.pvarki.fi"
+    req.get_subject().CN = "localmaeher.pvarki.fi"
     req.add_extensions(
         [
             crypto.X509Extension(b"keyUsage", True, b"digitalSignature,nonRepudiation,keyEncipherment"),
@@ -73,16 +79,84 @@ def csrfile(datadir: Path, keypair: crypto.PKey) -> Path:
 
 
 @pytest_asyncio.fixture(scope="module")
-async def mtlsfiles(csrfile: Path) -> Tuple[Path, Path, Path]:
+async def cafile(datadir: Path) -> Path:
+    """Get the CA chain from CFSSL and save"""
+    capath = datadir / "ca_public" / "mtlsca.pem"
+    cadir = capath.parent
+    cadir.mkdir(parents=True)
+    capem = (await get_ca()).replace("\\n", "\n")
+    LOGGER.debug("capem={}".format(capem))
+    capath.write_text(capem)
+    return capath
+
+
+@pytest_asyncio.fixture(scope="module")
+async def mtlsfiles(csrfile: Path, cafile: Path) -> Tuple[Path, Path, Path]:
     """Return cert, key and ca cert paths, this will sign the CSR again every time due to fixture scoping issues"""
     privkeypath = csrfile.parent.parent / "private" / "mtlsclient.key"
     assert privkeypath.exists()
     certpath = csrfile.parent / "mtlsclient.pem"
-    capath = csrfile.parent / "mtlsca.pem"
-    capem = (await get_ca()).replace("\\n", "\n")
-    LOGGER.debug("capem={}".format(capem))
     certpem = (await sign_csr(csrfile.read_text())).replace("\\n", "\n")
     LOGGER.debug("certpem={}".format(certpem))
-    capath.write_text(capem)
     certpath.write_text(certpem)
-    return certpath, privkeypath, capath
+    return certpath, privkeypath, cafile
+
+
+@pytest_asyncio.fixture(scope="module")
+async def servertlsfiles(datadir: Path) -> Tuple[Path, Path]:
+    """Generate a keypair, CSR"""
+    privkeypath = datadir / "private" / "mtlsserver.key"
+    pubkeypath = datadir / "public" / "mtlsserver.pub"
+    certpath = datadir / "public" / "mtlsserver.pem"
+    ckp = crypto.PKey()
+    LOGGER.debug("Generating keypair, this will take a moment")
+    ckp.generate_key(crypto.TYPE_RSA, 4096)
+    LOGGER.debug("Done")
+    with privkeypath.open("wb") as fpntr:
+        fpntr.write(crypto.dump_privatekey(crypto.FILETYPE_PEM, ckp))
+    with pubkeypath.open("wb") as fpntr:
+        fpntr.write(crypto.dump_publickey(crypto.FILETYPE_PEM, ckp))
+    csrpath = datadir / "public" / "mtlsserver.csr"
+    req = crypto.X509Req()
+    req.get_subject().CN = "fake.localmaeher.pvarki.fi"
+    req.add_extensions(
+        [
+            crypto.X509Extension(b"keyUsage", True, b"digitalSignature,nonRepudiation,keyEncipherment"),
+        ]
+    )
+    req.set_pubkey(ckp)
+    req.sign(ckp, "sha256")
+    with csrpath.open("wb") as fpntr:
+        fpntr.write(crypto.dump_certificate_request(crypto.FILETYPE_PEM, req))
+    certpem = (await sign_csr(csrpath.read_text())).replace("\\n", "\n")
+    certpath.write_text(certpem)
+
+    return certpath, privkeypath
+
+
+@pytest.fixture(scope="module")
+def httpserver_ssl_context(servertlsfiles: Tuple[Path, Path], cafile: Path) -> ssl.SSLContext:
+    """Create SSL context for pytest-httpserver"""
+    ssl_ctx = get_ssl_context(servertlsfiles, cafile.parent)
+    ssl_ctx.verify_mode = ssl.CERT_REQUIRED
+    return ssl_ctx
+
+
+@pytest.fixture()
+def mtlsclient(mtlsfiles: Tuple[Path, Path, Path]) -> aiohttp.ClientSession:
+    """fixture for client session with correct ssl context"""
+    certpath, privkeypath, capath = mtlsfiles
+    client = get_session((certpath, privkeypath), capath.parent)
+    return client
+
+
+@pytest.fixture(scope="module")
+def make_httpserver(httpserver_ssl_context: ssl.SSLContext) -> Generator[HTTPServer, None, None]:
+    """Module scoped server fixture"""
+    port = random.randint(10000, 64000)  # nosec
+    server = HTTPServer(host="fake.localmaeher.pvarki.fi", port=port, ssl_context=httpserver_ssl_context)
+    server.start()  # type: ignore
+    yield server
+    server.clear()  # type: ignore
+    if server.is_running():
+        server.stop()  # type: ignore
--- tests/tlstests/test_productapi.py
+++ tests/tlstests/test_productapi.py
@@ -1,5 +1,23 @@
 """Test product API/mTLS client things"""
 import logging
 
+import pytest
+import aiohttp
+from pytest_httpserver import HTTPServer
 
 LOGGER = logging.getLogger(__name__)
+
+
+@pytest.mark.skip(reason="Something is fucky-wucky with pytest-httpserver and tls")
+@pytest.mark.asyncio
+async def test_hello(httpserver: HTTPServer, mtlsclient: aiohttp.ClientSession) -> None:
+    """Quick and dirty test of the mTLS client and server"""
+    httpserver.expect_request("/").respond_with_data("hello world!")
+    url = httpserver.url_for("/")
+    LOGGER.debug("url={}".format(url))
+    async with mtlsclient.get(url) as result:
+        assert (await result.text()) == "hello world!"
+        assert result.connection
+        assert result.connection.transport
+        cert = result.connection.transport.get_extra_info("peercert")
+        assert cert
