c6a2c20488ac2710713a64d580548565106eca7e
Eero af Heurlin <eero.afheurlin@iki.fi>
2023-09-04 23:52:12 +0300

    use the correct way (lifespan context) to handle mtls init on fastapi startup

--- src/rasenmaeher_api/web/application.py
+++ src/rasenmaeher_api/web/application.py
@@ -1,6 +1,7 @@
 """Main API entrypoint"""
+from typing import AsyncGenerator
 import logging
-import asyncio
+from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 from libpvarki.logging import init_logging
@@ -13,11 +14,22 @@ from rasenmaeher_api.mtlsinit import mtls_init
 LOGGER = logging.getLogger(__name__)
 
 
+@asynccontextmanager
+async def app_lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
+    """Handle lifespan management things, like mTLS client init"""
+    # init
+    _ = app
+    await mtls_init()
+    # App runs
+    yield
+    # Cleanup
+
+
 def get_app() -> FastAPI:
     """Returns the FastAPI application."""
     init_logging(settings.log_level_int)
-    asyncio.run_coroutine_threadsafe(mtls_init(), asyncio.get_event_loop())
-    app = FastAPI(docs_url="/api/docs", openapi_url="/api/openapi.json")
+
+    app = FastAPI(docs_url="/api/docs", openapi_url="/api/openapi.json", lifespan=app_lifespan)
     app.include_router(router=api_router, prefix="/api/v1")
 
     LOGGER.info("API init done, setting log verbosity to '{}'.".format(settings.log_level))
