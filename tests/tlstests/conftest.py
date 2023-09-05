c770b7b00d60050aeb60acf3b0ef48a0812a1b94
Eero af Heurlin <eero.afheurlin@iki.fi>
2023-09-05 22:41:13 +0300

    remove pytest-httpserver, we will do this in docker

--- tests/tlstests/conftest.py
+++ tests/tlstests/conftest.py
@@ -1,17 +1,14 @@
 """mTLS fixtures"""
 from typing import Tuple, Generator
-import random
 from pathlib import Path
 import logging
 import asyncio
-import ssl
 
 import pytest
 import pytest_asyncio
 import aiohttp
 from OpenSSL import crypto  # FIXME: use cryptography instead of pyOpenSSL
-from libpvarki.mtlshelp import get_ssl_context, get_session
-from pytest_httpserver import HTTPServer
+from libpvarki.mtlshelp import get_session
 
 
 from rasenmaeher_api.web.api.product.views import sign_csr, get_ca
@@ -102,64 +99,9 @@ async def mtlsfiles(csrfile: Path, cafile: Path) -> Tuple[Path, Path, Path]:
     return certpath, privkeypath, cafile
 
 
-@pytest_asyncio.fixture(scope="module")
-async def servertlsfiles(datadir: Path) -> Tuple[Path, Path]:
-    """Generate a keypair, CSR"""
-    privkeypath = datadir / "private" / "mtlsserver.key"
-    pubkeypath = datadir / "public" / "mtlsserver.pub"
-    certpath = datadir / "public" / "mtlsserver.pem"
-    ckp = crypto.PKey()
-    LOGGER.debug("Generating keypair, this will take a moment")
-    ckp.generate_key(crypto.TYPE_RSA, 4096)
-    LOGGER.debug("Done")
-    with privkeypath.open("wb") as fpntr:
-        fpntr.write(crypto.dump_privatekey(crypto.FILETYPE_PEM, ckp))
-    with pubkeypath.open("wb") as fpntr:
-        fpntr.write(crypto.dump_publickey(crypto.FILETYPE_PEM, ckp))
-    csrpath = datadir / "public" / "mtlsserver.csr"
-    req = crypto.X509Req()
-    cnstr = "fake.localmaeher.pvarki.fi"
-    req.get_subject().CN = cnstr
-    req.add_extensions(
-        [
-            crypto.X509Extension(b"keyUsage", True, b"digitalSignature,nonRepudiation,keyEncipherment"),
-            crypto.X509Extension(b"extendedKeyUsage", True, b"serverAuth"),
-            crypto.X509Extension(b"subjectAltName", False, f"DNS:{cnstr}".encode("utf-8")),
-        ]
-    )
-    req.set_pubkey(ckp)
-    req.sign(ckp, "sha256")
-    with csrpath.open("wb") as fpntr:
-        fpntr.write(crypto.dump_certificate_request(crypto.FILETYPE_PEM, req))
-    certpem = (await sign_csr(csrpath.read_text())).replace("\\n", "\n")
-    certpath.write_text(certpem)
-
-    return certpath, privkeypath
-
-
-@pytest.fixture(scope="module")
-def httpserver_ssl_context(servertlsfiles: Tuple[Path, Path], cafile: Path) -> ssl.SSLContext:
-    """Create SSL context for pytest-httpserver"""
-    ssl_ctx = get_ssl_context(ssl.Purpose.CLIENT_AUTH, servertlsfiles, cafile.parent)
-    ssl_ctx.verify_mode = ssl.CERT_REQUIRED
-    return ssl_ctx
-
-
 @pytest.fixture()
 def mtlsclient(mtlsfiles: Tuple[Path, Path, Path]) -> aiohttp.ClientSession:
     """fixture for client session with correct ssl context"""
     certpath, privkeypath, capath = mtlsfiles
     client = get_session((certpath, privkeypath), capath.parent)
     return client
-
-
-@pytest.fixture(scope="module")
-def make_httpserver(httpserver_ssl_context: ssl.SSLContext) -> Generator[HTTPServer, None, None]:
-    """Module scoped server fixture"""
-    port = random.randint(10000, 64000)  # nosec
-    server = HTTPServer(host="fake.localmaeher.pvarki.fi", port=port, ssl_context=httpserver_ssl_context)
-    server.start()  # type: ignore
-    yield server
-    server.clear()  # type: ignore
-    if server.is_running():
-        server.stop()  # type: ignore
