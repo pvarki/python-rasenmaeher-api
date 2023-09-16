46addeb0a7ddb195e0efc2d1b23ac7cbc97e0cc7
Eero af Heurlin <eero.afheurlin@iki.fi>
2023-09-16 03:06:18 +0300

    start refactoring the test users to a fixture

--- src/rasenmaeher_api/sqlitedatabase.py
+++ src/rasenmaeher_api/sqlitedatabase.py
@@ -118,65 +118,6 @@ class SqliteDB:  # pylint: disable=too-few-public-methods
             )
             self.run_command(_q)
 
-            # Create development dummy roles
-            if self.settings.environment.lower() == "dev":
-                # Create test admin credentials
-                _q = self.settings.sqlite_insert_into_enrollment.format(
-                    work_id=settings.sqlite_init_testing_management_username,
-                    work_id_hash=settings.sqlite_init_testing_management_hash,
-                    state="ReadyForDelivery",
-                    accepted="somehashwhoaccepted_this",
-                    cert_dl_link="https://www.kuvaton.com/kuvei/asiakkaamme_kissa.jpg",
-                    cert_howto_dl_link="https://www.kuvaton.com/kuvei/asiakkaamme_kissa.jpg",
-                    mtls_test_link="https://www.kuvaton.com/kuvei/asiakkaamme_kissa.jpg",
-                    verification_code="",
-                    locked="",
-                )
-                self.run_command(_q)
-                _q = self.settings.sqlite_insert_into_management.format(
-                    management_hash=self.settings.sqlite_init_testing_management_hash, special_rules="enrollment"
-                )
-                self.run_command(_q)
-
-                # Create kissa dummy role
-                _q = self.settings.sqlite_insert_into_enrollment.format(
-                    work_id="kissa",
-                    work_id_hash="kissa123",
-                    state="ReadyForDelivery",
-                    accepted="somehashwhoaccepted_this",
-                    cert_dl_link="https://www.kuvaton.com/kuvei/asiakkaamme_kissa.jpg",
-                    cert_howto_dl_link="https://www.kuvaton.com/kuvei/asiakkaamme_kissa.jpg",
-                    mtls_test_link="https://www.kuvaton.com/kuvei/asiakkaamme_kissa.jpg",
-                    verification_code="",
-                    locked="",
-                )
-                self.run_command(_q)
-                # Create koira dummy role
-                _q = self.settings.sqlite_insert_into_enrollment.format(
-                    work_id="koira",
-                    work_id_hash="koira123",
-                    state="init",
-                    accepted="",
-                    cert_dl_link="",
-                    cert_howto_dl_link="",
-                    mtls_test_link="",
-                    verification_code="",
-                    locked="",
-                )
-                self.run_command(_q)
-                _q = self.settings.sqlite_insert_into_enrollment.format(
-                    work_id="porakoira",
-                    work_id_hash="porakoira123",
-                    state="init",
-                    accepted="",
-                    cert_dl_link="",
-                    cert_howto_dl_link="",
-                    mtls_test_link="",
-                    verification_code="",
-                    locked="",
-                )
-                self.run_command(_q)
-
         else:
             LOGGER.critical("Error! cannot create the database connection.")
 
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,5 @@
 """pytest automagics"""
-from typing import Dict, Any, AsyncGenerator, Generator
+from typing import Dict, Any, AsyncGenerator, Generator, Tuple, List
 import logging
 from pathlib import Path
 import uuid
@@ -19,6 +19,7 @@ from pytest_docker.plugin import Services
 from rasenmaeher_api.web.application import get_app
 from rasenmaeher_api.settings import settings
 from rasenmaeher_api.prodcutapihelpers import check_kraftwerk_manifest
+from rasenmaeher_api.sqlitedatabase import sqlite as sqlitewrapper
 
 init_logging(logging.DEBUG)
 LOGGER = logging.getLogger(__name__)
@@ -27,6 +28,86 @@ JWT_PATH = DATA_PATH / Path("jwt")
 
 
 # FIXME: Set environment so the mTLS client loads certs from a temp dir
+
+
+@pytest.fixture(scope="session")
+def test_user_secrets() -> Tuple[List[str], List[str]]:
+    """Create a few test users and work ids returns
+    list of work-ids and their corresponding "hashes"
+
+    First one has "enrollment" special role
+    """
+    # copied from the sqlitedb dev init so blame karppo :D
+    work_ids: List[str] = []
+    work_hashes: List[str] = []
+
+    # FIXME: do not put these to settings
+    work_ids.append(settings.sqlite_init_testing_management_username)
+    work_hashes.append(settings.sqlite_init_testing_management_hash)
+    # Create test admin credentials
+    _q = sqlitewrapper.settings.sqlite_insert_into_enrollment.format(
+        work_id=work_ids[-1],
+        work_id_hash=work_hashes[-1],
+        state="ReadyForDelivery",
+        accepted="somehashwhoaccepted_this",
+        cert_dl_link="https://www.kuvaton.com/kuvei/asiakkaamme_kissa.jpg",
+        cert_howto_dl_link="https://www.kuvaton.com/kuvei/asiakkaamme_kissa.jpg",
+        mtls_test_link="https://www.kuvaton.com/kuvei/asiakkaamme_kissa.jpg",
+        verification_code="",
+        locked="",
+    )
+    sqlitewrapper.run_command(_q)
+    _q = sqlitewrapper.settings.sqlite_insert_into_management.format(
+        management_hash=sqlitewrapper.settings.sqlite_init_testing_management_hash, special_rules="enrollment"
+    )
+    sqlitewrapper.run_command(_q)
+
+    work_ids.append("kissa")
+    work_hashes.append("kissa123")
+    # Create kissa dummy role
+    _q = sqlitewrapper.settings.sqlite_insert_into_enrollment.format(
+        work_id=work_ids[-1],
+        work_id_hash=work_hashes[-1],
+        state="ReadyForDelivery",
+        accepted="somehashwhoaccepted_this",
+        cert_dl_link="https://www.kuvaton.com/kuvei/asiakkaamme_kissa.jpg",
+        cert_howto_dl_link="https://www.kuvaton.com/kuvei/asiakkaamme_kissa.jpg",
+        mtls_test_link="https://www.kuvaton.com/kuvei/asiakkaamme_kissa.jpg",
+        verification_code="",
+        locked="",
+    )
+    sqlitewrapper.run_command(_q)
+
+    work_ids.append("koira")
+    work_hashes.append("koira123")
+    # Create koira dummy role
+    _q = sqlitewrapper.settings.sqlite_insert_into_enrollment.format(
+        work_id=work_ids[-1],
+        work_id_hash=work_hashes[-1],
+        state="init",
+        accepted="",
+        cert_dl_link="",
+        cert_howto_dl_link="",
+        mtls_test_link="",
+        verification_code="",
+        locked="",
+    )
+    sqlitewrapper.run_command(_q)
+    work_ids.append("koira")
+    work_hashes.append("koira123")
+    _q = sqlitewrapper.settings.sqlite_insert_into_enrollment.format(
+        work_id=work_ids[-1],
+        work_id_hash=work_hashes[-1],
+        state="init",
+        accepted="",
+        cert_dl_link="",
+        cert_howto_dl_link="",
+        mtls_test_link="",
+        verification_code="",
+        locked="",
+    )
+    sqlitewrapper.run_command(_q)
+    return work_ids, work_hashes
 
 
 # pylint: disable=redefined-outer-name
