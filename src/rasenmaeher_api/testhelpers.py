4d9af6f7fbcaf9a807bd1df852734a37962ed10b
Ari Karhunen <ari.karhunen@tietoevry.com>
2023-09-17 12:10:33 +0300

    default jwt updates
    typo fix

--- src/rasenmaeher_api/testhelpers.py
+++ src/rasenmaeher_api/testhelpers.py
@@ -72,18 +72,30 @@ def create_test_users() -> Tuple[List[str], List[str]]:
         locked="",
     )
     sqlitewrapper.run_command(_q)
-    work_ids.append("koira")
-    work_hashes.append("koira123")
-    _q = sqlitewrapper.settings.sqlite_insert_into_enrollment.format(
-        work_id=work_ids[-1],
-        work_id_hash=work_hashes[-1],
-        state="init",
-        accepted="",
-        cert_dl_link="",
-        cert_howto_dl_link="",
-        mtls_test_link="",
-        verification_code="",
-        locked="",
+
+    # Add initial 'first time user' hash that has permission to create admin users
+    _q = sqlitewrapper.settings.sqlite_insert_into_management.format(
+        management_hash=sqlitewrapper.settings.sqlite_first_time_user_hash, special_rules="first-user", active=1
     )
     sqlitewrapper.run_command(_q)
+
+    # Add the 'first time user' code to jwt table if not there already
+    _exchange_code = "HackHackHacatoneillaOnPorakoiraNukkumassa"
+    _q = sqlitewrapper.settings.sqlite_jwt_sel_from_jwt_where_exchange.format(exchange_code=_exchange_code)
+    _, _result = sqlitewrapper.run_command(_q)
+
+    if len(_result) == 0:
+        _q = sqlitewrapper.settings.sqlite_insert_into_jwt.format(
+            claims='{"anon_admin_session":true}',
+            consumed="no",
+            work_id_hash="NA",
+            work_id="NA",
+            exchange_code=_exchange_code,
+        )
+    else:
+        _q = sqlitewrapper.settings.sqlite_jwt_update_consumed_where_exchange.format(
+            new_consumed_state="no", exchange_code=_exchange_code
+        )
+    sqlitewrapper.run_command(_q)
+
     return work_ids, work_hashes
