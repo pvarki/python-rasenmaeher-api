44b7892f43f848ad11eca0bdc3670bb502d0203e
Eero af Heurlin <eero.afheurlin@iki.fi>
2023-09-24 23:27:37 +0300

    move PFX file creation to the Person class

--- src/rasenmaeher_api/web/api/enduserpfx/views.py
+++ src/rasenmaeher_api/web/api/enduserpfx/views.py
@@ -1,86 +1,13 @@
 """Enduser API views."""
 import logging
-import os
-import json
-from typing import cast, Dict, Any
 
-import OpenSSL.crypto
-import requests
 from fastapi import APIRouter, Request, Response, HTTPException, Depends
-from fastapi.responses import JSONResponse
 from multikeyjwt.middleware import JWTBearer, JWTPayload
-from ....settings import settings
+
+from ....db import Person
 
 router = APIRouter()
-
 LOGGER = logging.getLogger(__name__)
-
-
-async def pem_to_pfx(pem_key: str, pem_cert: str) -> bytes:
-    """Convert PEM data to PFX (PKCS12)."""
-    pfx = OpenSSL.crypto.PKCS12()
-    pfx.set_privatekey(OpenSSL.crypto.load_privatekey(OpenSSL.crypto.FILETYPE_PEM, bytes(pem_key, "UTF-8")))
-    pfx.set_certificate(OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_PEM, bytes(pem_cert, "UTF-8")))
-    pfxdata = pfx.export()
-    return pfxdata
-
-
-async def new_key(callsign: str) -> Dict[Any, Any]:
-    """pfx"""
-    url = f"{settings.cfssl_host}:{settings.cfssl_port}/api/v1/cfssl/newkey"
-
-    payload = json.dumps(
-        {
-            "hosts": [callsign],
-            "CN": callsign,
-            "names": [{"C": "FI", "ST": "Helsinki", "L": "Etela-Suomi", "O": "Puolustusvoimat"}],
-            "key": {"algo": "rsa", "size": 4096},
-        }
-    )
-    headers = {"Content-Type": "application/json"}
-
-    response = requests.request("POST", url, headers=headers, data=payload, timeout=10)
-    data = response.json().get("result")
-    return cast(Dict[Any, Any], data)
-
-
-async def sign_csr(csr: str) -> str:
-    """
-    Quick and dirty method to sign CSR from CFSSL
-    params: csr
-    returns: certificate
-    """
-    url = f"{settings.cfssl_host}:{settings.cfssl_port}/api/v1/cfssl/sign"
-
-    payload = json.dumps({"certificate_request": csr})
-    headers = {"Content-Type": "application/json"}
-
-    response = requests.request("POST", url, headers=headers, data=payload, timeout=5)
-    data = response.json().get("result").get("certificate")
-
-    return cast(str, data)
-
-
-async def save_key(pem_key: str, callsign: str) -> None:
-    """Save key to file."""
-    try:
-        if not os.path.exists(f"/data/persistent/{callsign}"):
-            os.makedirs(f"/data/persistent/{callsign}")
-        with open(f"/data/persistent/{callsign}/{callsign}.pem", "w", encoding="UTF-8") as file:
-            file.write(pem_key)
-    except OSError as _e:
-        LOGGER.info("Error: %s : %s", _e.filename, _e.strerror)
-
-
-async def save_pfx(pfx: bytes, callsign: str) -> None:
-    """Save PFX to file."""
-    try:
-        if not os.path.exists(f"/data/persistent/{callsign}"):
-            os.makedirs(f"/data/persistent/{callsign}")
-        with open(f"/data/persistent/{callsign}/{callsign}.pfx", "wb") as file:
-            file.write(pfx)
-    except OSError as _e:
-        LOGGER.info("Error: %s : %s", _e.filename, _e.strerror)
 
 
 def check_jwt(jwt: JWTPayload, callsign: str) -> bool:
@@ -113,15 +40,9 @@ async def return_enduser_certs(
 
     # Check that the JWT has sub claim and it mathes requested callsign
     check_jwt(jwt, callsign)
-
-    _newkeybundle = await new_key(callsign)
-    _key = cast(str, _newkeybundle.get("private_key"))
-    _csr = cast(str, _newkeybundle.get("certificate_request"))
-    _cert = await sign_csr(_csr)
-    _pfx = await pem_to_pfx(_key, _cert)
-    await save_key(_key, callsign)
-    await save_pfx(_pfx, callsign)
-    return Response(content=_pfx, media_type="application/x-pkcs12")
+    # Get the person (it will raise NotFound which is 404 HTTPException as well if callsign is not valid)
+    person = await Person.by_callsign(callsign)
+    return Response(content=person.pfxfile.read_bytes(), media_type="application/x-pkcs12")
 
 
 @router.get("/{callsign}")
@@ -143,9 +64,6 @@ async def check_enduser_bundle_available(
 
     # Check that the JWT has sub claim and it mathes requested callsign
     check_jwt(jwt, callsign)
-
-    if os.path.exists(f"/data/persistent/{callsign}/{callsign}.pfx"):  # pylint: disable=R1705
-        with open(f"/data/persistent/{callsign}/{callsign}.pfx", "rb") as file:
-            _pfx = file.read()
-        return Response(content=_pfx, media_type="application/x-pkcs12")
-    return JSONResponse(content={"result": "Error bundle not found"}, media_type="application/json", status_code=404)
+    # Get the person (it will raise NotFound which is 404 HTTPException as well if callsign is not valid)
+    person = await Person.by_callsign(callsign)
+    return Response(content=person.pfxfile.read_bytes(), media_type="application/x-pkcs12")
