8fae074bfbffd6f3b1988b45746a741002168652
Eero af Heurlin <eero.afheurlin@iki.fi>
2023-09-24 23:48:10 +0300

    Make sure we have at least one revoked cert when loading CRLs

--- tests/test_cfssl.py
+++ tests/test_cfssl.py
@@ -1,15 +1,22 @@
 """Test CFSSL wrappers"""
 import logging
+import uuid
 
 import pytest
 import cryptography.x509
+import pytest_asyncio
 from async_asgi_testclient import TestClient  # pylint: disable=import-error
 
 
 from rasenmaeher_api.cfssl.public import get_ca, get_crl
 from rasenmaeher_api.cfssl.private import validate_reason
+from rasenmaeher_api.db import Person
+
+from .test_db import ginosession  # pylint: disable=W0611
 
 LOGGER = logging.getLogger(__name__)
+
+# pylint: disable=W0621
 
 
 @pytest.mark.asyncio
@@ -19,9 +26,20 @@ async def test_get_ca() -> None:
     assert capem.startswith("-----BEGIN CERTIFICATE-----")
 
 
+@pytest_asyncio.fixture(scope="session")
+async def one_revoked_cert(ginosession: None) -> None:
+    """Make sure we have at least one revoked cert"""
+    _ = ginosession
+    # We have to make actual certs to be able to revoke them
+    person = await Person.create_with_cert(str(uuid.uuid4()))
+    await person.revoke("key_compromise")
+
+
 @pytest.mark.asyncio
-async def test_get_crl() -> None:
+async def test_get_crl(one_revoked_cert: None) -> None:
     """Test CA fetching"""
+    # Make sure there is at least one revoked cert
+    _ = one_revoked_cert
     crl_der = await get_crl()
     crl = cryptography.x509.load_der_x509_crl(crl_der)
     assert crl
@@ -39,8 +57,10 @@ def test_reasons() -> None:
 
 
 @pytest.mark.asyncio
-async def test_crl_route(unauth_client: TestClient) -> None:
+async def test_crl_route(unauth_client: TestClient, one_revoked_cert: None) -> None:
     """Check that we can get a parseable CRL from the route"""
+    # Make sure there is at least one revoked cert
+    _ = one_revoked_cert
     client = unauth_client
     resp = await client.get("/api/v1/utils/crl")
     resp.raise_for_status()
