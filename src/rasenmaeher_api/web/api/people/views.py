aa09c9dbc998abfb930b33079ca969b2d679569e
Aciid <703382+Aciid@users.noreply.github.com>
2023-09-30 19:40:52 +0300

    Refactor people/listing

--- src/rasenmaeher_api/web/api/people/views.py
+++ src/rasenmaeher_api/web/api/people/views.py
@@ -1,15 +1,16 @@
 """People API views."""  # pylint: disable=too-many-lines
-from typing import List, Union
+from typing import List
 import logging
 
 
-from fastapi import APIRouter, Request, Depends, HTTPException
+from fastapi import APIRouter, Depends
 
 from .schema import (
     CallSignPerson,
     PeopleListOut,
 )
 from ..middleware.mtls import MTLSorJWT
+from ..middleware.user import ValidUser
 from ....db import Person
 
 LOGGER = logging.getLogger(__name__)
@@ -17,43 +18,15 @@ LOGGER = logging.getLogger(__name__)
 router = APIRouter(dependencies=[Depends(MTLSorJWT(auto_error=True))])
 
 
-async def check_management_permissions(
-    raise_exeption: bool = True, callsign: str = "", required_role: str = "admin"
-) -> Union[bool, None]:
-    """
-    Simple function to check if requester has requested role.
-    """
-
-    user = await Person.by_callsign(callsign=callsign)
-    is_admin = await user.has_role(role=required_role)
-
-    # Raise exeption if
-    if raise_exeption and is_admin is False:
-        _reason = "Error. User doesn't have required permissions. See system logs."
-        LOGGER.error(
-            "Missing role from user : '{}'. Required permissions that are missing : '{}'".format(
-                callsign, required_role
-            )
-        )
-        LOGGER.error("{}".format(_reason))
-        raise HTTPException(status_code=403, detail=_reason)
-
-    return is_admin
-
-
-@router.get("/list", response_model=PeopleListOut)
-async def request_people_list(
-    request: Request,
-) -> PeopleListOut:
+@router.get(
+    "/list", response_model=PeopleListOut, dependencies=[Depends(ValidUser(auto_error=True, require_roles=["admin"]))]
+)
+async def request_people_list() -> PeopleListOut:
     """
     /list
     Return people/list.
     Returns a list of dicts, callsign_list = [ {  "callsign":'x', "roles": ["str"] 'extra':'x' } ]
     """
-
-    await check_management_permissions(
-        raise_exeption=True, callsign=request.state.mtls_or_jwt.userid, required_role="admin"
-    )
 
     _people_list = Person.list()
 
