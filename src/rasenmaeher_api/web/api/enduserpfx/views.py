3d90f050873019cc4d0716b156e8bd27d5144333
Eero af Heurlin <eero.afheurlin@iki.fi>
2023-10-01 00:46:03 +0300

    make it possible to have the .pfx on the url when fetching pfx, add test for the two cases

--- src/rasenmaeher_api/web/api/enduserpfx/views.py
+++ src/rasenmaeher_api/web/api/enduserpfx/views.py
@@ -1,71 +1,31 @@
 """Enduser API views."""
 import logging
 
-from fastapi import APIRouter, Request, Response, HTTPException, Depends
-from multikeyjwt.middleware import JWTBearer, JWTPayload
+from fastapi import APIRouter, HTTPException, Depends
+from fastapi.responses import FileResponse
 
 from ....db import Person
+from ..middleware.user import ValidUser
 
 router = APIRouter()
 LOGGER = logging.getLogger(__name__)
 
 
-def check_jwt(jwt: JWTPayload, callsign: str) -> bool:
-    """Sanity checks for JWT payload here"""
-    if not jwt.get("sub", False):
-        LOGGER.error("Requesting JWT must have sub claim")
-        raise HTTPException(status_code=403, detail="Forbidden")
-
-    if jwt.get("sub") == callsign:
-        return True
-    LOGGER.error("Requesting JWT sub claim doesn't match requested callsign")
-    raise HTTPException(status_code=403, detail="Forbidden")
-
-
-@router.post("/{callsign}")
-async def return_enduser_certs(
-    request: Request,
-    callsign: str,
-    jwt: JWTPayload = Depends(JWTBearer(auto_error=True)),
-) -> Response:
-    """
-    Method to create key, sign CSR and return PFX
-    :param callsign: OTTER1
-    :returns pfx
-    """
-    if callsign is None or callsign == "":
-        _reason = "Error. Callsign missing as last parameter"
-        LOGGER.error("{} : {}".format(request.url, _reason))
-        raise HTTPException(status_code=400, detail=_reason)
-    LOGGER.debug("Called with callsign={}".format(callsign))
-
-    # Check that the JWT has sub claim and it mathes requested callsign
-    check_jwt(jwt, callsign)
-    # Get the person (it will raise NotFound which is 404 HTTPException as well if callsign is not valid)
-    person = await Person.by_callsign(callsign)
-    return Response(content=person.pfxfile.read_bytes(), media_type="application/x-pkcs12")
-
-
+@router.get("/{callsign}.pfx")
 @router.get("/{callsign}")
-async def check_enduser_bundle_available(
-    request: Request,
+async def get_user_pfx(
     callsign: str,
-    jwt: JWTPayload = Depends(JWTBearer(auto_error=True)),
-) -> Response:
+    person: Person = Depends(ValidUser(auto_error=True)),
+) -> FileResponse:
     """
     Method to check if bundle is available
-    :param callsign: OTTER1
-    :returns pfx or 404 error
+    :param callsign: OTTER1.pfx
+    :returns pfx or 403 error
     """
-
-    if callsign is None or callsign == "":
-        _reason = "Error. Callsign missing as last parameter"
-        LOGGER.error("{} : {}".format(request.url, _reason))
-        raise HTTPException(status_code=400, detail=_reason)
+    if callsign.endswith(".pfx"):
+        callsign = callsign[:-4]
     LOGGER.debug("Called with callsign={}".format(callsign))
+    if person.callsign != callsign:
+        raise HTTPException(status_code=403, detail="Callsign must match authenticated user")
 
-    # Check that the JWT has sub claim and it mathes requested callsign
-    check_jwt(jwt, callsign)
-    # Get the person (it will raise NotFound which is 404 HTTPException as well if callsign is not valid)
-    person = await Person.by_callsign(callsign)
-    return Response(content=person.pfxfile.read_bytes(), media_type="application/x-pkcs12")
+    return FileResponse(path=person.pfxfile, media_type="application/x-pkcs12", filename=f"{callsign}.pfx")
---
+++ tests/usersapis/test_userinfos.py
@@ -0,0 +1,22 @@
+"""Test users own info things"""
+import logging
+
+import pytest
+from async_asgi_testclient import TestClient  # pylint: disable=import-error
+from cryptography.hazmat.primitives.serialization import pkcs12
+
+LOGGER = logging.getLogger(__name__)
+
+
+@pytest.mark.parametrize("fname", ["ENROLLUSERa", "ENROLLUSERa.pfx"])
+@pytest.mark.asyncio
+async def test_user_pfx(user_mtls_client: TestClient, fname: str) -> None:
+    """Test that we can get the pfx"""
+    client = user_mtls_client
+    url = f"/api/v1/enduserpfx/{fname}"
+    resp = await client.get(url)
+    LOGGER.debug(resp)
+    resp.raise_for_status()
+    pfxdata = pkcs12.load_pkcs12(resp.content, None)
+    assert pfxdata.key
+    assert pfxdata.cert
