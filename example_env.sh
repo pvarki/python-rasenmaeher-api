568366df047a5e2fefd7743302268cad965b332c
Eero af Heurlin <eero.afheurlin@iki.fi>
2023-10-01 18:52:52 +0300

    example environment for production compose (needed for pre-commit check too)

---
+++ example_env.sh
@@ -0,0 +1,8 @@
+export KEYCLOAK_DATABASE_PASSWORD="koira"  # pragma: allowlist secret
+export RM_DATABASE_PASSWORD="kissa"  # pragma: allowlist secret
+export POSTGRES_PASSWORD="kani"  # pragma: allowlist secret
+export LDAP_ADMIN_PASSWORD="pupu"  # pragma: allowlist secret
+export KEYCLOAK_ADMIN_PASSWORD="marsu"  # pragma: allowlist secret
+export KEYCLOAK_MANAGEMENT_PASSWORD="alpakka"  # pragma: allowlist secret
+export SERVER_DOMAIN="sleepy-sloth.pvarki.fi"
+export CFSSL_CA_NAME="sleepy-sloth"
