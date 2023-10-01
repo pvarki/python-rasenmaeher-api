8c3af66372e33bd4eacacc0cc848f743051a8d97
Eero af Heurlin <eero.afheurlin@iki.fi>
2023-10-01 16:13:19 +0300

    Support the new user file fragment world

--- src/rasenmaeher_api/web/api/instructions/schema.py
+++ src/rasenmaeher_api/web/api/instructions/schema.py
@@ -3,6 +3,7 @@ from typing import Dict, Optional
 
 from pydantic import BaseModel, Extra, Field
 from libpvarki.schemas.product import UserInstructionFragment
+from pydantic_collections import BaseCollectionModel
 
 
 class AllProdcutsInstructionFragments(BaseModel):  # pylint: disable=too-few-public-methods
@@ -29,3 +30,38 @@ class AllProdcutsInstructionFragments(BaseModel):  # pylint: disable=too-few-pub
                 },
             ],
         }
+
+
+class ProductFile(BaseModel):  # pylint: disable=too-few-public-methods
+    """File description"""
+
+    title: str = Field(description="Title for the file")
+    filename: str = Field(description="file name")
+    data: str = Field(description="data-url for the file")
+
+    class Config:  # pylint: disable=too-few-public-methods
+        """Example values for schema"""
+
+        extra = Extra.forbid
+
+
+class ProductFileList(BaseCollectionModel[ProductFile]):  # type: ignore[misc]  # pylint: disable=too-few-public-methods
+    """List of files"""
+
+    class Config:  # pylint: disable=too-few-public-methods
+        """Pydantic configs"""
+
+        extra = Extra.forbid
+
+
+class AllProdcutsInstructionFiles(BaseModel):  # pylint: disable=too-few-public-methods
+    """user files for all products"""
+
+    files: Dict[str, Optional[ProductFileList]] = Field(
+        description="files keyed by product short name, if fetching failed value for that product is null"  # pylint: disable=C0301
+    )
+
+    class Config:  # pylint: disable=too-few-public-methods
+        """Example values for schema"""
+
+        extra = Extra.forbid
