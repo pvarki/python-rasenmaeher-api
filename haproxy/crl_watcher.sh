c2af6979909b6e5932d4dd9d42a35648833e65e6
Eero af Heurlin <eero.afheurlin@iki.fi>
2024-01-28 20:05:04 +0200

    we cannot both check mTLS cert validity and passthrough TLS at the same time, also it seems TAKServer at least can auto-reload the CRL when the file changes

--- haproxy/crl_watcher.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-CRL_FILE=/ca_public/crl.pem
-echo "Waiting for "$CRL_FILE
-while inotifywait -e modify,move,create,delete "$CRL_FILE"; do
-    echo "CRL file changed. Reloading HAProxy..."
-    pkill -SIGUSR2 haproxy
-done
