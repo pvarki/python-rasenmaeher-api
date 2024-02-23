3aa1c374e9815edc362cc5038f489b3a322ea20e
Eero af Heurlin <eero.afheurlin@iki.fi>
2024-02-23 21:06:42 +0200

    Handle productapi calls as fire-and-forget tasks

--- src/rasenmaeher_api/db/people.py
+++ src/rasenmaeher_api/db/people.py
@@ -284,14 +284,8 @@ class Role(DBModel):  # type: ignore[misc] # pylint: disable=R0903
 async def post_user_crud(userinfo: UserCRUDRequest, endpoint_suffix: str) -> None:
     """Wrapper to be more DRY in the basic CRUD things"""
     endpoint = f"api/v1/users/{endpoint_suffix}"
-
-    responses = await post_to_all_products(
-        endpoint,
-        userinfo.dict(),
-        OperationResultResponse,
-    )
-    LOGGER.debug("got responses: {}".format(responses))
-    # TODO: Check responses and log errors
+    # We can't do anything about any issues with the responses so don't collect them
+    await post_to_all_products(endpoint, userinfo.dict(), OperationResultResponse, collect_responses=False)
 
 
 async def user_created(person: Person) -> None:
