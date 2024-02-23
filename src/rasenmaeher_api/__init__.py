2d605c0d32c1c84abfba84fd76b9d5049d68bc23
Eero af Heurlin <eero.afheurlin@iki.fi>
2024-02-23 22:28:56 +0200

    bump version

--- src/rasenmaeher_api/__init__.py
+++ src/rasenmaeher_api/__init__.py
@@ -1,2 +1,2 @@
 """ python-rasenmaeher-api """
-__version__ = "1.2.0"  # NOTE Use `bump2version --config-file patch` to bump versions correctly
+__version__ = "1.3.0"  # NOTE Use `bump2version --config-file patch` to bump versions correctly
--- tests/test_rasenmaeher_api.py
+++ tests/test_rasenmaeher_api.py
@@ -15,7 +15,7 @@ from rasenmaeher_api.rmsettings import RMSettings
 
 def test_version() -> None:
     """Make sure version matches expected"""
-    assert __version__ == "1.2.0"
+    assert __version__ == "1.3.0"
 
 
 @pytest.mark.asyncio
