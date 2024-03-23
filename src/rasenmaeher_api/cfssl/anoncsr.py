8d5b7435bbeac1063deb71c71bf7904c357bea33
Eero af Heurlin <eero.afheurlin@iki.fi>
2024-03-23 11:46:12 +0200

    use ocsprest for signing the first anon cert too

--- src/rasenmaeher_api/cfssl/anoncsr.py
+++ src/rasenmaeher_api/cfssl/anoncsr.py
@@ -3,13 +3,14 @@ import logging
 
 import aiohttp
 
-from .base import base_url, anon_session, get_result_cert, CFSSLError
+
+from .base import anon_session, get_result_cert, CFSSLError, ocsprest_base
 from ..rmsettings import RMSettings
 
 LOGGER = logging.getLogger(__name__)
 
 
-async def anon_sign_csr(csr: str) -> str:
+async def anon_sign_csr(csr: str, bundle: bool = True) -> str:
     """
     Quick and dirty method to sign CSR from CFSSL.
 
@@ -19,10 +20,11 @@ async def anon_sign_csr(csr: str) -> str:
     returns: certificate
     """
     async with (await anon_session()) as session:
-        url = f"{base_url()}/api/v1/cfssl/sign"
-        payload = {"certificate_request": csr}
+        url = f"{ocsprest_base()}/api/v1/csr/sign"
+        payload = {"certificate_request": csr, "profile": "client", "bundle": bundle}
         try:
             async with session.post(url, json=payload, timeout=RMSettings.singleton().cfssl_timeout) as response:
-                return await get_result_cert(response)
+                resp = await get_result_cert(response)
+                return resp
         except aiohttp.ClientError as exc:
             raise CFSSLError(str(exc)) from exc
