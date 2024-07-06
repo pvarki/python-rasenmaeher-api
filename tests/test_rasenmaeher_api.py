06533f4cc5ebaf6cb95e1861b5027c7d7e3c19d4
Eero af Heurlin <eero.afheurlin@iki.fi>
2024-07-06 17:27:17 +0300

    bump version

--- tests/test_rasenmaeher_api.py
+++ tests/test_rasenmaeher_api.py
@@ -15,7 +15,7 @@ from rasenmaeher_api.rmsettings import RMSettings
 
 def test_version() -> None:
     """Make sure version matches expected"""
-    assert __version__ == "1.4.0"
+    assert __version__ == "1.5.0"
 
 
 @pytest.mark.asyncio(scope="session")
