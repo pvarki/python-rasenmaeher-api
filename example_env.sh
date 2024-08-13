b4f858d8a4fc65a26551c0d879d2b50b3005d9dc
Benjam <53127823+benjamsf@users.noreply.github.com>
2024-08-13 19:14:31 +0300

    Add new env variable to compose
    VITE_ASSET_SET will tell ui what asset set to use. "fdf" for FDF set, "neutral" for the 'pvatk' theme (logos, copyright notices, etc.).

--- example_env.sh
+++ example_env.sh
@@ -11,3 +11,4 @@ export MW_LE_EMAIL="example@example.com"
 export MW_LE_TEST="true"  # switch to false when you are ready for production
 export TAKSERVER_CERT_PASS="KissaKoira123!AlpakkaMursu"  # used for the JKS
 export TAK_CA_PASS="AlpakkaMursu!KissaKoira123"  # used for the JKS
+export VITE_ASSET_SET="fdf"  # used RMUI to define asset sets (logos, etc).
