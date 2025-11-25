f2f4eb55ff87a79578c7cc92a77c2538c771deba
Sami J. Lehtinen <sjl@iki.fi>
2025-11-25 09:54:20 +0200

    feat: allow passing JWT signing pubkey in env

--- docker/container-init.sh
+++ docker/container-init.sh
@@ -13,6 +13,11 @@ echo "$(getent ahostsv4 host.docker.internal | awk '{ print $1 }') mtx.localmaeh
 test -d /data/persistent/private || ( mkdir -p /data/persistent/private && chmod og-rwx /data/persistent/private )
 test -d /data/persistent/public || mkdir -p /data/persistent/public
 
+# Handle external JWT public key if provided
+if [ -n "$EXTERNAL_JWT_PUBKEY_B64" ]; then
+    echo "$EXTERNAL_JWT_PUBKEY_B64" | base64 -d > ${JWT_PUBKEY_PATH:-/data/persistent/public}/external.pub
+fi
+
 # Copy JWT public keys
 if [ -d /pvarki/publickeys ]
 then
