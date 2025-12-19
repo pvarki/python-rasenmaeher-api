c4e7084c9b721aa478941c789bf3b09c177fb59e
logaritmi <228653256+logaritmi@users.noreply.github.com>
2025-12-19 10:34:55 +0200

    Revert "feat: add airguardian host"
    This reverts commit f976bd817d72645c2766878efc9d749404491126.

--- docker/container-init.sh
+++ docker/container-init.sh
@@ -8,7 +8,6 @@ echo "$(getent ahostsv4 host.docker.internal | awk '{ print $1 }') tak.localmaeh
 echo "$(getent ahostsv4 host.docker.internal | awk '{ print $1 }') kc.localmaeher.dev.pvarki.fi" >>/etc/hosts
 echo "$(getent ahostsv4 host.docker.internal | awk '{ print $1 }') bl.localmaeher.dev.pvarki.fi" >>/etc/hosts
 echo "$(getent ahostsv4 host.docker.internal | awk '{ print $1 }') mtx.localmaeher.dev.pvarki.fi" >>/etc/hosts
-echo "$(getent ahostsv4 host.docker.internal | awk '{ print $1 }') airguardian.localmaeher.dev.pvarki.fi" >>/etc/hosts
 
 # Make sure the persistent directories exist
 test -d /data/persistent/private || ( mkdir -p /data/persistent/private && chmod og-rwx /data/persistent/private )
