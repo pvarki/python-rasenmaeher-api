b31a1f3573ed2222d82f34be609231c040bba7af
Eero af Heurlin <eero.afheurlin@iki.fi>
2026-05-23 17:51:20 +0300

    chore: update build number

--- tests/test_rasenmaeher_api.py
+++ tests/test_rasenmaeher_api.py
@@ -16,7 +16,7 @@ from rasenmaeher_api.rmsettings import RMSettings
 
 def test_version() -> None:
     """Make sure version matches expected"""
-    assert __version__ == "1.16.0+260508"
+    assert __version__ == "1.16.0+260523"
 
 
 @pytest.mark.asyncio(loop_scope="session")
