1ce67eb469998f02e6e8226b1c8755bf61df6c7b
Akseli Lehtniemi <60463103+Akaseli@users.noreply.github.com>
2026-06-26 08:36:38 +0300

    chore: bump version

--- tests/test_rasenmaeher_api.py
+++ tests/test_rasenmaeher_api.py
@@ -16,7 +16,7 @@ from rasenmaeher_api.rmsettings import RMSettings
 
 def test_version() -> None:
     """Make sure version matches expected"""
-    assert __version__ == "1.17.6+260613"
+    assert __version__ == "1.17.7+260626"
 
 
 @pytest.mark.asyncio(loop_scope="session")
