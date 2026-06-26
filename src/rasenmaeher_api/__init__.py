d863c7312f09f54d2c7405f9d671ab45f738bc87
Akseli Lehtniemi <60463103+Akaseli@users.noreply.github.com>
2026-06-26 08:36:38 +0300

    chore: bump version

--- src/rasenmaeher_api/__init__.py
+++ src/rasenmaeher_api/__init__.py
@@ -1,3 +1,3 @@
 """python-rasenmaeher-api"""
 
-__version__ = "1.17.6+260613"  # NOTE Use `bump-my-version` to bump versions correctly
+__version__ = "1.17.7+260626"  # NOTE Use `bump-my-version` to bump versions correctly
