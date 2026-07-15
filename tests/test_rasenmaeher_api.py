42a200dadb88909babc5f5ea9d964f8a95ec16b7
Niilo Vuokila <59707802+niilovuo@users.noreply.github.com>
2026-07-15 12:20:28 +0300

    chore: bump version to 1.18.0+260715

--- tests/test_rasenmaeher_api.py
+++ tests/test_rasenmaeher_api.py
@@ -16,7 +16,7 @@ from rasenmaeher_api.rmsettings import RMSettings
 
 def test_version() -> None:
     """Make sure version matches expected"""
-    assert __version__ == "1.17.8+260702"
+    assert __version__ == "1.18.0+260715"
 
 
 @pytest.mark.asyncio(loop_scope="session")
