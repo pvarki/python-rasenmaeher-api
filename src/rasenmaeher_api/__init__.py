923fd5ec5ae4c89a8481fe6de9e0feb76a29cbe7
Niilo Vuokila <59707802+niilovuo@users.noreply.github.com>
2026-07-28 12:02:29 +0300

    chore: bump version to 1.18.1+260728

--- src/rasenmaeher_api/__init__.py
+++ src/rasenmaeher_api/__init__.py
@@ -1,3 +1,3 @@
 """python-rasenmaeher-api"""
 
-__version__ = "1.18.0+260715"  # NOTE Use `bump-my-version` to bump versions correctly
+__version__ = "1.18.1+260728"  # NOTE Use `bump-my-version` to bump versions correctly
