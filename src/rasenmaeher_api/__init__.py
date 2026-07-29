4e778e552a66815938a1eef271994249fda5206b
mazkdevf <79049205+mazkdevf@users.noreply.github.com>
2026-07-29 13:39:49 +0300

    chore: bump version to 1.19.0+260729

--- src/rasenmaeher_api/__init__.py
+++ src/rasenmaeher_api/__init__.py
@@ -1,3 +1,3 @@
 """python-rasenmaeher-api"""
 
-__version__ = "1.18.1+260728"  # NOTE Use `bump-my-version` to bump versions correctly
+__version__ = "1.19.0+260729"  # NOTE Use `bump-my-version` to bump versions correctly
