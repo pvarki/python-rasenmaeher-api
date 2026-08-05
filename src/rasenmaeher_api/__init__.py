b764a32b65aa565d1b6bded2d3da415bf3cb7450
Niilo Vuokila <59707802+niilovuo@users.noreply.github.com>
2026-08-05 09:52:45 +0300

    chore: bump version to 1.18.2+260805

--- src/rasenmaeher_api/__init__.py
+++ src/rasenmaeher_api/__init__.py
@@ -1,3 +1,3 @@
 """python-rasenmaeher-api"""
 
-__version__ = "1.18.1+260728"  # NOTE Use `bump-my-version` to bump versions correctly
+__version__ = "1.18.2+260805"  # NOTE Use `bump-my-version` to bump versions correctly
--- tests/test_rasenmaeher_api.py
+++ tests/test_rasenmaeher_api.py
@@ -15,7 +15,7 @@ from rasenmaeher_api.rmsettings import RMSettings
 
 def test_version() -> None:
     """Make sure version matches expected"""
-    assert __version__ == "1.18.1+260728"
+    assert __version__ == "1.18.2+260805"
 
 
 @pytest.mark.asyncio(loop_scope="session")
