58c80c9112e2489c05950a593e5db0a0ebdd09c4
Eero af Heurlin <rambo@iki.fi>
2026-08-23 03:07:41 +0300

    chore: bump version

--- src/rasenmaeher_api/__init__.py
+++ src/rasenmaeher_api/__init__.py
@@ -1,3 +1,3 @@
 """python-rasenmaeher-api"""
 
-__version__ = "1.19.0+260822"  # NOTE Use `bump-my-version` to bump versions correctly
+__version__ = "1.19.1+260823"  # NOTE Use `bump-my-version` to bump versions correctly
